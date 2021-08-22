bc855d0f0b47b13829dd899807af73b7beaa803a
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-08-22 14:20:32 +0700

    add port

--- public/js/server.js
+++ public/js/server.js
@@ -1,7 +1,7 @@
 const localv1 = "http://localhost:8006/";
-const localol = "https://my-api-movie.herokuapp.com/apis/v1/";
+const localol = "https://clonenippon.herokuapp.com/";
 
 export const getAllProduct = async (loai) => {
-  let a = await fetch(`${localv1}getproduct?loai=${loai}`);
+  let a = await fetch(`${localol}getproduct?loai=${loai}`);
   return await a.json();
 };
